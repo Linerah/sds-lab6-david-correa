2f9c7c9c71800caba5cfa6c3caf9a4fb471b6127
Traven Reese <82552048+Travenedu@users.noreply.github.com>
2022-03-23 00:40:55 -0400

    Update
    Added partner names

--- flask-mongo-template/app.py
+++ flask-mongo-template/app.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#Partners: Traven Reese & Kevin R. Linera Hernandez
+
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
